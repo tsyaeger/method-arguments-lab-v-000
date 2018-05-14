b965e8b35dd19d84c22c239b142e5a7e4f9cf40a
taryn yaeger <tsyaeger@gmail.com>
2018-05-14 02:41:57 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,5 +1,8 @@
 
 
 def introduction(name)
-  puts "Hi, my name is #{}."
+  puts "Hi, my name is #{name}."
 end
+
+def introduction_with_language(name,language)
+"Hi, my name is #{name} and I am learning to program in #{language}.\n"
